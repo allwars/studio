f41ab66c7f19fc561cd6edbbb2fc9f6b42cd0a06
alvaro martin “allwars” <alvaromb8@gmail.com>
2025-07-15 11:48:44 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -52,7 +52,7 @@ export default function LoginPage() {
                 </div>
                 <div className="relative">
                   <Lock className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-muted-foreground" />
-                  <Input id="password" type="password" required className="pl-10" />
+                  <Input id="password" type="password" required className="pl-10" suppressHydrationWarning />
                 </div>
               </div>
               <Button type="submit" className="w-full bg-primary hover:bg-primary/90">
